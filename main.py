b71c8728d10eed547eefb8846e08cd6c43db9ed1
Alexander Kern <alexgit@alexkern.de>
2022-05-02 17:51:42 +0200

    funktioniert endlich

--- main.py
+++ main.py
@@ -9,12 +9,25 @@ from PySide6.QtCore import QObject
 from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtWebEngineWidgets import QWebEngineView
 
+def fkt(var):
+    print("bla "+str(var))
+
+class myApp(QGuiApplication):
+    pass
+    #def __init__():
+    #    self.runJavaScript("document.documentElement.innerHTML=\"<html><head></head><body><h1>testx</h1></body></html>\";alert(\"2\"+document.documentElement.innerHTML)", fkt)
+    #def exec(self):
+    #    #super().exec()
+
 if __name__ == "__main__":
-    app = QGuiApplication(sys.argv)
+    app =myApp(sys.argv)
     engine = QQmlApplicationEngine()
     engine.load(os.fspath(Path(__file__).resolve().parent / "main.qml"))
     w = engine.rootObjects()[0].children()[1]
-    print(str((w.property("url"))))
+    print(str((w.__str__())))
+    print(str(dir(w)))
+    #w.show()
+    # w.runJavaScript("document.documentElement.innerHTML=\"<html><head></head><body><h1>testx</h1></body></html>\";alert(\"2\"+document.documentElement.innerHTML)", fkt)
     if not engine.rootObjects():
         sys.exit(-1)
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
--- main.qml
+++ main.qml
@@ -6,12 +6,12 @@ Window {
     width: 640
     height: 480
     visible: true
-    title: qsTr("Hello World")
+    title: qsTr("ReTa")
     id : win
     WebEngineView {
         id : web
         visible: true
         anchors.fill: parent
-        url: "https://www.qt.io"
+        url: "file:///home/alex/religionen.html"
     }
 }
