ce9368cbb616e9cbe863473678bf4385f0fe2b31
Alexander Kern <alexgit@alexkern.de>
2022-05-02 15:01:47 +0200

    funktioniert endlich

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.10
+# This Python file uses the following encoding: utf-8
+import os
+from pathlib import Path
+import sys
+
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtWebEngineWidgets import QWebEngineView
+
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    engine.load(os.fspath(Path(__file__).resolve().parent / "main.qml"))
+    if not engine.rootObjects():
+        sys.exit(-1)
+    sys.exit(app.exec_())
