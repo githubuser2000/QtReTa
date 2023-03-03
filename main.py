3b7f6a075fafcef88654b0360138d0ba05db1e0a
Alexander Kern <alexgit@alexkern.de>
2023-03-03 17:51:23 +0100

    keine störenden ausgaben mehr

--- main.py
+++ main.py
@@ -2,29 +2,36 @@
 # This Python file uses the following encoding: utf-8
 # import multiprocessing
 import os
-#import random
+# import random
 import re
-#import string
+# import string
 import sys
 # from pathlib import Path
 # home = str(Path.home())
 # import tempfile
 from pathlib import Path
 
+# import PySide6.QtCore
 # import zstd
-from PySide6.QtCore import QObject, Slot
+from PySide6.QtCore import QLoggingCategory, QObject, QUrl, Slot
 from PySide6.QtGui import QGuiApplication, QIcon
 from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtWebEngineWidgets import QWebEngineView
-from PySide6.QtWidgets import QSystemTrayIcon
+from PySide6.QtWidgets import QApplication, QMainWindow, QSystemTrayIcon
+
+# import ress
+
+# print(dir(PySide6.QtCore))
+# exit
+# sys.stderr = open(os.devnull, "w")
 
 # tempdir = tempfile.gettempdir()
-#import binascii
+# import binascii
 
-import ress
-#from data import base
 
-randstr = ""
+# from data import base
+
+# randstr = ""
 
 
 def windows_to_browser_path(path):
@@ -51,11 +58,21 @@ def fkt(var):
     print("bla " + str(var))
 
 
+def ifWebAddr(input_str: str) -> bool:
+    regex = r"^https?://(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}(?:/?|[/?]\S+)$|^(?:\d{1,3}\.){3}\d{1,3}$"
+    # regex basiert auf: https://stackoverflow.com/a/3809435/15698617
+
+    if re.match(regex, input_str):
+        return True
+    else:
+        return False
+
+
 class MyAppEng(QQmlApplicationEngine):
-    @Slot()
-    def entf(self):
-        global randstr
-        # os.remove(tempdir + os.sep + randstr + ".html")
+    # @Slot()
+    # def entf(self):
+    # global randstr
+    # os.remove(tempdir + os.sep + randstr + ".html")
 
     def __init__(self):
         super().__init__()
@@ -63,12 +80,12 @@ class MyAppEng(QQmlApplicationEngine):
 
 
 def start():
-    global randstr
+    # global randstr
     app = QGuiApplication(sys.argv)
     engine = MyAppEng()
     engine.rootContext().setContextProperty("MyAppEng", engine)
     # app.setWindowIcon(QIcon(":/Jupiter.png"))
-    #print(":"+os.fspath(Path(__file__).resolve().parent / "Jupiter.png"))
+    # print(":"+os.fspath(Path(__file__).resolve().parent / "Jupiter.png"))
     app.setWindowIcon(QIcon(os.fspath(Path(__file__).resolve().parent / "Jupiter.png")))
     # comp = b""
     # with open('/home/alex/religionen.html', "rb") as f:
@@ -97,9 +114,13 @@ def start():
     )
     pfad = ""
     browser_path = "file:///home/alex/religionen.html?preselect=no_universal"
+    tueb = 0
     for path in sys.argv[1:]:
         if path_regex.match(path):
             pfad = path
+        elif ifWebAddr(path):
+            tueb = 1
+            browser_path = path
     if pfad != "":
         windows_path_regex = re.compile(
             r'^[a-zA-Z]:\\(?:[^\\/:*?"<>|\r\n]+\\)*[^\\/:*?"<>|\r\n]*$'
@@ -107,15 +128,20 @@ def start():
         linux_path_regex = re.compile(r"^/.*$")
         if windows_path_regex.match(pfad):
             browser_path = windows_to_browser_path(pfad)
+            tueb = 2
         elif linux_path_regex.match(pfad):
             browser_path = linux_to_browser_path(pfad)
+            tueb = 3
+    print("browser address: {}".format(browser_path))
     w.setProperty("url", browser_path)
     if not engine.rootObjects():
         sys.exit(-1)
     if "-tray" in sys.argv:
         engine.rootObjects()[0].setVisible(False)
-    else:
-        print("possible parameters: -tray")
+    elif tueb == 0:
+        print(
+            "possible parameters: -tray\nAnd web addresses or filesystem adresses for windows or unix"
+        )
     # icon = QIcon("Jupiter.png")
     # tray = QSystemTrayIcon()
     # tray.setIcon(icon)
@@ -129,5 +155,8 @@ def start():
     sys.exit(onexi)
 
 
+# QT_LOGGING_RULES = "*.info=False;driver.usb.debug=True"
 if __name__ == "__main__":
+    QLoggingCategory.setFilterRules("*.error=true\n*.info=false\n*.warning=false")
+    # web_view = QWebEngineView()
     start()
